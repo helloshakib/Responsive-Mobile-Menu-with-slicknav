b3148a1fa2c18002a68c773cec01b8742b62f709
Md Shakib Hasan <30505740+helloshakib@users.noreply.github.com>
2018-04-03 00:23:38 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+  (function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($){
+        
+        $("ul#navigation").slicknav({
+            prependTo: ".responsive-menu-wrap"
+        });
+
+    });
+
+
+    jQuery(window).load(function(){
+       
+    });
+
+
+}(jQuery));	
